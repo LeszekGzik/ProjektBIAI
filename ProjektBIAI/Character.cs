77eb85d2a052f69234f6e0c7eefb99af82277032
Leszek Gzik (leszgzi982) <leszgzi982@student.polsl.pl>
2017-04-07 20:47:10 +0200

    sum shitz

--- ProjektBIAI/Character.cs
+++ ProjektBIAI/Character.cs
@@ -8,5 +8,16 @@ namespace ProjektBIAI
 {
     class Character
     {
+        byte [] stats = new byte [9];
+        int currentHp;
+        int maxHp;     // HP - punkty zdrowia; bazowo 100 + 100 za punkt
+        int hpRegen; // regeneracja HP na rundę; 5 za punkt
+        int baseDmg;    // obrażenia; bazowo 10 + 20 za punkt
+        int critRate;   // szansa na trafienie krytyczne; bazowo 0% + 2% za punkt
+        double critDmg;  // dodatkowe obrażenia zadawane przez trafienie krytyczne; bazowo +50% +5% za punkt
+        int hitRate;  // szansa na trafienie; bazowo 50% + 5% za punkt
+        int dodgeRate; // szansa na uniknięcie trafienia; -5% za punkt
+        int blockRate; // szansa na zablokowanie; 0% +2% za punkt
+        double blockPower; // procent o jaki zmniejszone są obrażenia po udanym bloku; bazowo -50% -5% za punkt
     }
 }
